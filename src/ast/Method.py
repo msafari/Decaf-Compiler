f66a91dea0651f4a1430e91845f0729b937fd264
Maedeh Safari <maedeh.safari@priceline.com>
2017-08-21 23:28:57 -0400

    feat(ast): add Method definition

---
+++ src/ast/Method.py
@@ -0,0 +1,34 @@
+class Method:
+    """A class encoding methods and their attributes in Decaf"""
+    def __init__(self, mname, mclass, visibility, storage, rtype):
+        global lastmethod
+        self.name = mname
+        lastmethod += 1
+        self.id = lastmethod
+        self.inclass = mclass
+        self.visibility = visibility
+        self.storage = storage
+        self.rtype = rtype
+        self.vars = VarTable()
+        
+    def update_body(self, body):
+        self.body = body
+
+    def add_var(self, vname, vkind, vtype):
+        self.vars.add_var(vname, vkind, vtype)
+
+    def printout(self):
+        print "METHOD: {0}, {1}, {2}, {3}, {4}, {5}".format(self.id, self.name, self.inclass.name, self.visibility, self.storage, self.rtype)
+        print "Method Parameters:",
+        print ', '.join(["%d"%v.id for v in self.vars.get_params()])
+        self.vars.printout()
+        print "Method Body:"
+        self.body.printout()
+
+    def argtypes(self):
+        return [v.type for v in self.vars.get_params()]
+
+    def typecheck(self):
+        global current_method
+        current_method = self
+        self.body.typecheck()

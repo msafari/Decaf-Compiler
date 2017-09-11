d21912ec4d6f59cdf0aaa895b26a57e0cd3164bc
Maedeh Safari <maedeh.safari@gmail.com>
2017-09-11 19:20:43 -0400

    feat(Expressions): New Array Expr

---
+++ src/ast/Expressions/NewArrayExpr.py
@@ -0,0 +1,24 @@
+class NewArrayExpr(Expr):
+    def __init__(self, basetype, args, lines):
+        self.lines = lines
+        self.basetype = basetype
+        self.args = args
+        self.__typeof = None
+    def __repr__(self):
+        return "New-array({0}, {1})".format(self.basetype, self.args)
+
+    def typeof(self):
+        if (self.__typeof == None):
+            mytype = Type(self.basetype, len(self.args))
+            for a in self.args:
+                if (not a.typeof().isok()):
+                    # previous error, so mark and pass
+                    mytype = Type('error')
+                    break
+                if (not a.typeof().isint()):
+                    # int arg type expected
+                    signal_type_error("Type error in argument to New Array expression: int expected, found {0}".format(str(a.typeof())), a.lines)
+                    mytype = Type('error')
+                    break
+            self.__typeof = mytype
+        return self.__typeof

1b383ccfe1480ab95e602978fa671c2339b82d9a
Maedeh Safari <maedeh.safari@gmail.com>
2017-09-11 19:21:07 -0400

    feat(Expressions): New Object Expr

---
+++ src/ast/Expressions/NewObjectExpr.py
@@ -0,0 +1,25 @@
+class NewObjectExpr(Expr):
+    def __init__(self, cref, args, lines):
+        self.lines = lines
+        self.classref = cref
+        self.args = args
+        self.__typeof = None
+        self.objID = None
+    def __repr__(self):
+        return "New-object({0}, {1})".format(self.classref.name, self.args)
+
+    def typeof(self):
+        if (self.__typeof == None):
+            # resolve the constructor name first
+            argtypes = [a.typeof() for a in self.args]
+            if (all([a.isok() for a in argtypes])):
+                j = resolve_constructor(current_class, self.classref, argtypes, self.lines)
+                if (j == None):
+                    self.__typeof = Type('error')
+                else:
+                    self.constructor = j
+                    self.__typeof = Type(self.classref)
+            else:
+                # type error in some argument; already signaled before
+                self.__typeof = Type('error')
+        return self.__typeof

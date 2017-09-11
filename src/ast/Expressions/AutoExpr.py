1b05fd5bd6b421882ecc052092a0509340fd1f8c
Maedeh Safari <maedeh.safari@gmail.com>
2017-09-11 19:18:45 -0400

    feat(Expressions): AutoExpr

---
+++ src/ast/Expressions/AutoExpr.py
@@ -0,0 +1,20 @@
+class AutoExpr(Expr):
+    def __init__(self, arg, oper, when, lines):
+        self.lines = lines
+        self.arg = arg
+        self.oper = oper
+        self.when = when
+        self.__typeof = None
+    def __repr__(self):
+        return "Auto({0}, {1}, {2})".format(self.arg, self.oper, self.when)
+
+    def typeof(self):
+        if (self.__typeof == None):
+            argtype = self.arg.typeof()
+            if (argtype.isnumeric()):
+                self.__typeof = argtype
+            else:
+                self.__typeof = Type('error')
+                if (argtype.isok()):
+                    signal_type_error('Type error in auto expression: int/float expected, found {0}'.format(str(argtype)), self.lines)
+        return self.__typeof

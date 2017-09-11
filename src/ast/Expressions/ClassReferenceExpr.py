f17546544e11d7be08a59d0f5e19e2bdafd61c08
Maedeh Safari <maedeh.safari@gmail.com>
2017-09-11 19:19:26 -0400

    feat(Expressions): ClassReferenceExpr

---
+++ src/ast/Expressions/ClassReferenceExpr.py
@@ -0,0 +1,12 @@
+class ClassReferenceExpr(Expr):
+    def __init__(self, cref, lines):
+        self.lines = lines
+        self.classref = cref
+        self.__typeof = None
+    def __repr__(self):
+        return "ClassReference({0})".format(self.classref.name)
+
+    def typeof(self):
+        if (self.__typeof == None):
+            self.__typeof = Type(self.classref, literal=True)
+        return self.__typeof

c679cc1f48f8accc88a43974a32c416eca3df8eb
Maedeh Safari <maedeh.safari@gmail.com>
2017-09-11 19:18:15 -0400

    feat(Expressions): ArrayAccessExpr

---
+++ src/ast/Expressions/ArrayAccessExpr.py
@@ -0,0 +1,21 @@
+class ArrayAccessExpr(Expr):
+    def __init__(self, base, index, lines):
+        self.lines = lines
+        self.base = base
+        self.index = index
+        self.__typeof = None
+    def __repr__(self):
+        return "Array-access({0}, {1})".format(self.base, self.index)
+
+    def typeof(self):
+        if (self.__typeof == None):
+            if (not self.index.typeof().isint()):
+                signal_type_error("Type error in index of Array Index expression: integer expected, found {0}".format(str(self.index.typeof())), self.index.lines)
+                mytype = Type('error')
+            if (self.base.typeof().kind != 'array'):
+                signal_type_error("Type error in base of Array Index expression: array type expected, found {0}".format(str(self.base.typeof())), self.base.lines)
+                mytype = Type('error')
+            else:
+                mytype = self.base.typeof().basetype
+            self.__typeof = mytype
+        return self.__typeof
